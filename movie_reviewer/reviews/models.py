02b76538d105bbec0d1840ac8f895025ab13b656
Marsha Williamson <tramar92@hotmail.com>
2020-01-14 17:47:59 -0500

    added upvote functionality using @property decorator

--- movie_reviewer/reviews/models.py
+++ movie_reviewer/reviews/models.py
@@ -10,12 +10,19 @@ from django.utils import timezone
 
 
 class Review(models.Model):
-    critic = models.ForeignKey(Critic, on_delete=models.CASCADE)
+    critic = models.ForeignKey(Critic, on_delete=models.CASCADE, related_name='critic')
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     headline = models.CharField(max_length=100, null=True, blank=True)
     text = models.TextField(max_length=1000, null=True, blank=True)
     post_date = models.DateTimeField(default=timezone.now)
     recommend = models.BooleanField(default=True)
+    critics_who_upvoted = models.ManyToManyField(Critic)
 
+
+    @property
+    def number_of_upvotes(self):
+        return len(self.critics_who_upvoted)
+
+    
     def __str__(self):
         return f'Review by: {self.critic} movie: {self.movie}'
