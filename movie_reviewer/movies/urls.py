5ed111587ed7742b4fa8583a38b3e69684a46ccb
Elizabeth Scheidt <escheidt5@gmail.com>
2020-01-15 19:19:07 -0500

    fix missing urls

--- movie_reviewer/movies/urls.py
+++ movie_reviewer/movies/urls.py
@@ -3,5 +3,8 @@ from movie_reviewer.movies.views import (
     RecentMoviesView, MovieView, SearchMovieView, SearchResults)
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='homepage')
+    path('', RecentMoviesView.as_view(), name='homepage'),
+    path('movie/<int:id>/', MovieView.as_view(), name='movie detail'),
+    path('search/', SearchMovieView.as_view(), name='search'),
+    path('search_results/', SearchResults.as_view(), name='search results')
 ]
